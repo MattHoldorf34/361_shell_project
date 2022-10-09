f11fc1ddf78afe23bc2fa34cf372bde3866eec48
Thomas Oves <ovest@silber-courses.cis.udel.edu>
2022-10-09 23:35:25 +0000

    Added kill, started signal stuff, has error

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+
 #include "sh.h"
 #include <signal.h>
 #include <stdio.h>
@@ -6,14 +7,30 @@ void sig_handler(int signal);
 
 int main( int argc, char **argv, char **envp )
 {
-  /* put signal set up stuff here */
-
-  return sh(argc, argv, envp);
+	/* put signal set up stuff here */
+	signal(SIGINT, sig_handler);
+	signal(SIGTSTP, sig_handler);
+	signal(SIGTERM, sig_handler);
+	signal(EOF, sig_handler);
+	return sh(argc, argv, envp);
 }
 
 void sig_handler(int signal)
 {
-  /* define your signal handler */
+	/* define your signal handler */
+	if (signal == SIGINT) {
+		printf("\nCtrl+C caught\n");
+		if (getpid() == 0) {
+			kill(pid, 15);
+			printf("Killed");
+		} else printf("Signal ignored");
+	} else if (signal == SIGTSTP)
+		printf("\nCtrl+Z caught\n");
+	else if (signal == SIGTERM)
+		printf("\nKill Caught\n");
+	else if (signal == EOF)
+		printf("\nCtrl+D caught\n");
+	else return;
 }
 
 
