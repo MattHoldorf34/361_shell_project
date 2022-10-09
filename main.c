4e4308af41dce082916b30f4fc6b7070c57a9283
Thomas Oves <ovest@silber-courses.cis.udel.edu>
2022-10-09 23:53:21 +0000

    commented out what is causing error

--- main.c
+++ main.c
@@ -8,16 +8,19 @@ void sig_handler(int signal);
 int main( int argc, char **argv, char **envp )
 {
 	/* put signal set up stuff here */
+/*
 	signal(SIGINT, sig_handler);
 	signal(SIGTSTP, sig_handler);
 	signal(SIGTERM, sig_handler);
 	signal(EOF, sig_handler);
 	return sh(argc, argv, envp);
+*/
 }
 
 void sig_handler(int signal)
 {
 	/* define your signal handler */
+/*
 	if (signal == SIGINT) {
 		printf("\nCtrl+C caught\n");
 		if (getpid() == 0) {
@@ -31,6 +34,7 @@ void sig_handler(int signal)
 	else if (signal == EOF)
 		printf("\nCtrl+D caught\n");
 	else return;
+*/
 }
 
 
