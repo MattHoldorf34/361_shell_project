e3d006aa14f27dd0fbe7222a5c840d3f1352603c
Thomas Oves <ovest@silber-courses.cis.udel.edu>
2022-10-10 00:49:36 +0000

    tried to figure out ls

--- sh.h
+++ sh.h
@@ -1,13 +1,16 @@
-
+#include <glob.h>
 #include "get_path.h"
 
-extern int pid; //added extern
+int pid; //added extern
 int sh( int argc, char **argv, char **envp);
 char *which(char *command, struct pathelement *pathlist);
 char *where(char *command, struct pathelement *pathlist);
 void list ( char *dir );
 void printenv(char **envp);
+void execCommand(char *command, char **args, int status);
 void printExec(char * command);
+int findWildcard(char wc, char **args);
+void runGlob(int index, char *commandpath, struct pathelement *pathlist, char **args, glob_t globIn, int status);
 
 #define PROMPTMAX 32
 #define MAXARGS 10
